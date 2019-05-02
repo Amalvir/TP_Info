2b00196d7b8b413151e7bbb5d3ffbd4718473029
Amalvir <samalonsovirissel@gmail.com>
2019-05-02 11:20:53 +0200

    Ajout de TP13

--- TP13/TP13.py
+++ TP13/TP13.py
@@ -3,6 +3,7 @@ liste1 = [[0]*9]*9  # 9 fois la meme liste
 liste2 = [[1 for i in range(9)] for j in range(9)]  # Utiliser elle
 # print(liste2)
 
+
 def affiche_grille(grille):
     ligne = ""
     count1 = 0
@@ -24,12 +25,26 @@ def affiche_grille(grille):
             print("-"*12)
             count2 = 0
 
+
 def num_carre(i, j):
     a = i // 3
-
-    t = a+b
+    b = j // 3
+    t = 3*a + b
     return t
 
-for i in range(0, 9):
-    for j in range(0, 9):
-        print(num_carre(i, j))
+
+def case_carre(n, k):
+    return 3*n//3 + k//3, 3*(n % 3) + k % 3
+
+
+def est_complete(grille):
+    for i in grille:
+        try:
+            i.index(0)
+        except ValueError:
+            return False
+
+
+
+
+print(est_complete(liste2))
