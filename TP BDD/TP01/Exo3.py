a8fb712abc67220acdee8e6c50ea499711c64ffc
Amalvir <samalonsovirissel@gmail.com>
2019-06-06 10:41:07 +0200

    Fin BDD 1

--- TP BDD/TP01/Exo3.py
+++ TP BDD/TP01/Exo3.py
@@ -21,3 +21,24 @@ def requete(req):
     bdd.close()
 
     return liste_res
+
+
+req1 = "SELECT X FROM etab_sco"
+req2 = "SELECT Y FROM etab_sco"
+
+abscisses = requete(req1)
+ordonnee = requete(req2)
+
+les_x, les_y = [], []
+
+print(type(abscisses[1][0]))
+for i in range(len(abscisses)):
+
+    if type(abscisses[i][0]) == float and type(ordonnee[i][0]) == float and ordonnee[i][0] <= 7200000 \
+            and ordonnee[i][0] >= 6000000:
+        les_x.append(abscisses[i][0])
+        les_y.append(ordonnee[i][0])
+
+
+plt.plot(les_x, les_y, 'b.')
+plt.show()
